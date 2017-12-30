7d74467c327a641236ea8f163c8c8b3155a86839
eric <ericstrong89@gmail.com>
2017-12-30 16:44:45 +0000

    changes

--- app/src/main/java/com/example/eric/justjava/MainActivity.java
+++ app/src/main/java/com/example/eric/justjava/MainActivity.java
@@ -2,11 +2,16 @@ package com.example.eric.justjava;
 
 import java.text.NumberFormat;
 import java.util.Locale;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -15,12 +20,19 @@ public class MainActivity extends AppCompatActivity {
     private Button button = null;
     private TextView quantity = null;
     private TextView price = null;
+    private TextView orderSummary = null;
     private Button buttonAdd = null;
     private Button buttonSub = null;
+    private CheckBox whippedCream = null;
+    private CheckBox chocolate = null;
+    private EditText cusname = null;
+
 
     public int num = 0;
     public double total = 0.00;
     public double pricePerItem = 3.50;
+
+    public String greeting = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,53 +43,125 @@ public class MainActivity extends AppCompatActivity {
         button = (Button) findViewById(R.id.button);
         quantity = (TextView) findViewById(R.id.quantity);
         price = (TextView) findViewById(R.id.price);
+        orderSummary = (TextView) findViewById(R.id.orderSumary);
         buttonAdd = (Button) findViewById(R.id.addButton);
         buttonSub = (Button) findViewById(R.id.subButton);
+        whippedCream = (CheckBox) findViewById(R.id.whippedCream);
+        chocolate = (CheckBox) findViewById(R.id.chocolate);
+        cusname = (EditText) findViewById(R.id.name);
 
     }
 
-    public void addQuantity(View view){
+    public void addQuantity(View view) {
 
-        num ++;
+
+        num++;
+
         total = total + pricePerItem;
-        price.setText("Total " + NumberFormat.getCurrencyInstance(Locale.UK).format(total) + "\nThank you!");
-        Log.d("eric","" + total);
+        price.setText(NumberFormat.getCurrencyInstance(Locale.UK).format(total));
+        price.setTextColor(Color.GREEN);
+        Log.d("eric", "" + total);
         quantity.setText("" + num);
+        quantity.setTextColor(Color.RED);
+        orderSummary.setText("Price");
+
 
     }
 
-    public void subQuantity(View view){
+    public void subQuantity(View view) {
 
-        num --;
+        num--;
+        orderSummary.setText("Price");
 
-        if(num < 0){
+        if (num < 0) {
             num = 0;
             total = 0.00;
-            price.setText("Total " + NumberFormat.getCurrencyInstance(Locale.UK).format(total));
+
+
+
+            price.setText(NumberFormat.getCurrencyInstance(Locale.UK).format(total));
             Toast.makeText(MainActivity.this, "you can only select > 0 Quantity",
                     Toast.LENGTH_LONG).show();
 
-        }else{
+        } else {
+
             total = total - pricePerItem;
-            price.setText("Total " + NumberFormat.getCurrencyInstance(Locale.UK).format(total));
+            price.setText(NumberFormat.getCurrencyInstance(Locale.UK).format(total));
+            price.setTextColor(Color.RED);
         }
 
 
-        Log.d("eric","" + total);
+        Log.d("eric", "" + total);
         quantity.setText("" + num);
 
     }
 
-    public void orderSubmit(View view){
-        if(num == 0){
+    public void orderSubmit(View view) {
+        if (num == 0) {
             Toast.makeText(MainActivity.this, "you have not yet selected any quantity",
                     Toast.LENGTH_LONG).show();
-        }else{
-            Toast.makeText(MainActivity.this, "your order is " + num + " Coffees. Total " + NumberFormat.getCurrencyInstance(Locale.UK).format(total),
+        } else {
+
+
+            String name = "Name ";
+
+            name += cusname.getText().toString();
+            String orderQuant = "Quantity: " + num;
+            String whipped = "";
+            String choc = "";
+
+            if (whippedCream.isChecked()) {
+                whipped = "\nWhipped Cream: " + whippedCream.isChecked();
+                //total += 0.50;
+            } else {
+                whipped = "\nWhipped Cream: " + whippedCream.isChecked();
+            }
+
+            if (chocolate.isChecked()) {
+                choc = "\nChocolate: " + chocolate.isChecked();
+                //total += 0.60;
+            } else {
+                choc = "\nChocolate: " + chocolate.isChecked();
+            }
+
+
+            greeting = "" + name + "\n" + orderQuant + "\nTotal:" + NumberFormat.getCurrencyInstance(Locale.UK).format(total) + whipped + choc + "\nThank you!!!";
+
+
+            orderSummary.setText("Order Summary");
+            Toast.makeText(MainActivity.this, greeting,
                     Toast.LENGTH_LONG).show();
+
         }
 
+    }
+
+    public void cream(View view){
+
+        boolean addCream = whippedCream.isChecked();
+        if(addCream){
+            total += 0.50;
+            price.setText(NumberFormat.getCurrencyInstance(Locale.UK).format(total));
+
+        }else{
+            total -= 0.50;
+            price.setText(NumberFormat.getCurrencyInstance(Locale.UK).format(total));
+        }
 
     }
+
+    public void chocolate(View view){
+        boolean addChoc = chocolate.isChecked();
+        if(addChoc){
+            total += 0.60;
+            price.setText(NumberFormat.getCurrencyInstance(Locale.UK).format(total));
+
+        }else{
+            total -= 0.60;
+            price.setText(NumberFormat.getCurrencyInstance(Locale.UK).format(total));
+        }
+
+    }
+
 
 }
